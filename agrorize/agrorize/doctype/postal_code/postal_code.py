1a64270c7e0e3cbedab6f73aec8bd847ea399e5c
InshaSiS Technologies <136191740+Inshasis@users.noreply.github.com>
2026-04-21 19:28:54 +0530

    farmer active + postal code upercase show

--- agrorize/agrorize/doctype/postal_code/postal_code.py
+++ agrorize/agrorize/doctype/postal_code/postal_code.py
@@ -1,9 +1,21 @@
-# Copyright (c) 2026, hidayatali and contributors
+# Copyright (c) 2026, Inshasis and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class PostalCode(Document):
-	pass
+    def validate(self):
+        self.convert_to_uppercase()
+    
+    def before_save(self):
+        self.convert_to_uppercase()
+    
+    def convert_to_uppercase(self):
+        fields_to_convert = ['post', 'taluka', 'district', 'state']
+        
+        for field in fields_to_convert:
+            if self.get(field):
+                self.set(field, self.get(field).upper())
+    
+    
